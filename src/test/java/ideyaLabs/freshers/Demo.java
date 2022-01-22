3fd60327f9cbb144104c3a9fd202594253b6b578
vishnu <vishnug@ideyalabs.com>
2022-01-22 17:09:40 +0530

    Updated code

--- src/test/java/ideyaLabs/freshers/Demo.java
+++ src/test/java/ideyaLabs/freshers/Demo.java
@@ -1,5 +1,18 @@
 package ideyaLabs.freshers;
 
 public class Demo {
-	
+	private void add() {
+		int a=10;
+		int b= 20;
+		int c=30;
+		int result=a+b+c;
+		System.out.println(result);
+		
+	}
+	public static void main(String[] args)
+	{
+		new Demo().add();
+		/*Demo d = new Demo();
+		d.add();*/
+	}
 }
